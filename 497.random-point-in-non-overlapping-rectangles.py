e3a8a17eba01986a5dd89fdb34ec403faeba73ba
Atlas975 <adilw2002@gmail.com>
2023-01-10 22:31:16 +0400

    Auto commit 18:31:16-01/10/23 (UTC)

---
+++ 497.random-point-in-non-overlapping-rectangles.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=497 lang=python3
+#
+# [497] Random Point in Non-overlapping Rectangles
+#
+
+# @lc code=start
+class Solution:
+
+    def __init__(self, rects: List[List[int]]):
+        
+
+    def pick(self) -> List[int]:
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(rects)
+# param_1 = obj.pick()
+# @lc code=end
+
---
+++ 99.recover-binary-search-tree.py
@@ -0,0 +1,45 @@
+#
+# @lc app=leetcode id=99 lang=python3
+#
+# [99] Recover Binary Search Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+
+
+class Solution:
+    def recoverTree(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+
+        n1, n2 = None, None
+        pre = TreeNode(float('-inf'))
+        s = deque()
+
+        while root or s:
+            while root:
+                s.append(root)
+                root = root.left
+            root = s.pop()
+
+            if pre.val > root.val:
+                if not n1:
+                    n1 = pre
+                if n1:
+                    n2 = root
+
+            pre = root
+            root = root.right
+
+        n1.val, n2.val = n2.val, n1.val
+
+# @lc code=end
+

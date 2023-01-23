43e29a7e360b729d94d2b208d38c8e243c96e413
Atlas975 <adilw2002@gmail.com>
2023-01-23 11:10:27 +0000

    Auto commit 11:10:27-01/23/23 (UTC)

--- 20.valid-parentheses.rs
+++ 20.valid-parentheses.rs
@@ -5,9 +5,20 @@
  */
 
 // @lc code=start
+use std::collections::VecDeque;
 impl Solution {
     pub fn is_valid(s: String) -> bool {
-        
+        let mut stack = VecDeque::new();
+        for c in s.chars() {
+            match c {
+                '{' => stack.push_back('}'),
+                '(' => stack.push_back(')'),
+                '[' => stack.push_back(']'),
+                '}' | ')' | ']' if Some(c) != stack.pop_back() => return false,
+                _ => (),
+            }
+        }
+        stack.is_empty()
     }
 }
 // @lc code=end

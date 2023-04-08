5dc4002fca44962a72b4a69d01a179ea019fdd2f
Adil Wazeer <adilw2002@gmail.com>
2023-04-08 15:01:19 +0400

    Auto commit 11:01:19 08/04/23 (UTC)

--- 45.jump-game-ii.rs
+++ 45.jump-game-ii.rs
@@ -7,6 +7,19 @@
 // @lc code=start
 impl Solution {
     pub fn jump(nums: Vec<i32>) -> i32 {
+        let mut jumps = 0;
+        let mut current_jump_end = 0;
+        let mut farthest = 0;
+        let lstidx = nums.len() - 1;
+        while current_jump_end < lstidx {
+            let tmp = farthest;
+            farthest = (current_jump_end..=tmp)
+                .map(|i| i + nums[i] as usize)
+                .max()
+                .unwrap();
+            current_jump_end = tmp + 1;
+            jumps += 1;
+        jumps
         
     }
 }
